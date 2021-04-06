68e6e974bc33dad1950f29249a4067f8c7a0bf21
Richard Ebanks <richard.ebanks@mymona.uwi.edu>
2021-04-06 17:06:50 -0400

    commit

--- app/views.py
+++ app/views.py
@@ -6,7 +6,10 @@ This file creates your application.
 """
 
 from app import app
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, flash
+from flask_wtf import Flaskform 
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 
 ###
@@ -23,6 +26,41 @@ def home():
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Our Recipe Planner")
+@app.route('/search/')
+def search():
+    return render_template('search.html')
+
+@app.route('/AddRecipe/')
+def addRecipe():
+    return render_template('addRecipe.html')
+@app.route('/recipe')
+def viewRecipe():
+    return render_template('viewRecipe.html')
+@app.route('/login/')
+def login():
+       error = None
+    if request.method == 'POST':
+    # this part need to change as we get the sql
+        if request.form['username'] != app.config['ADMIN_USERNAME'] or request.form['password'] != app.config['ADMIN_PASSWORD']:
+            error = 'Invalid username or password'
+        else:
+            session['logged_in'] = True
+            
+            flash('You were logged in', 'success')
+            return redirect(url_for('upload'))
+    return render_template('login.html', error=error)
+@app.route('/logout/')
+def logout():
+    render_template('logout.html')
+    
+@app.route('/signup/')
+def signUp():
+    return render_template('sign_up.html')
+
+@app.route('/newMealPlan/')
+def createMealPlan():
+    return render_template('newMealPlan')
+
 
 
 ###
@@ -61,6 +99,14 @@ def add_header(response):
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
+
+'''connect to mysql database'''
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'meal_planner'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+mysql.init_app(app)
 
 
 if __name__ == '__main__':
